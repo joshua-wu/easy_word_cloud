c9e35792d8fdc3e856c862d6a4e92a1086c2fb93
jianwu <wujian.mailbox@gmail.com>
2014-07-20 09:25:23 +0800

    a little clean

--- examples/simple.py
+++ examples/simple.py
@@ -12,21 +12,21 @@ best_elements = None
 best_score = 0
 
 #test case 2
-text = open(path.join(d, 'examples/constitution.txt')).read()
+text = open(path.join(d, '../examples/constitution.txt')).read()
 words = easywordcloud.process_text(text)
 words = [(word, math.sqrt(weight)) for word, weight in words]  # to makes the weight more even
-easywordcloud.draw_word_cloud(words, width, height, path.join(d, 'examples/constitution.png'))
+easywordcloud.draw_word_cloud(words, width, height, path.join(d, '../examples/constitution.png'))
 
 
 #test case 2
-text = open(path.join(d, 'examples/alice.txt')).read()
+text = open(path.join(d, '../examples/alice.txt')).read()
 words = easywordcloud.process_text(text)
-easywordcloud.draw_word_cloud(words, width, height, path.join(d, 'examples/alice.png'), None)
+easywordcloud.draw_word_cloud(words, width, height, path.join(d, '../examples/alice.png'), None)
 
 #test case 3
 words = [(u'ワード', 0.7), (u'クラウド', 0.6)]
-easywordcloud.draw_word_cloud(words, width, height, path.join(d, 'examples/japanese.png'), '/Library/Fonts/Andale Mono.ttf')
+easywordcloud.draw_word_cloud(words, width, height, path.join(d, '../examples/japanese.png'), '/Library/Fonts/Andale Mono.ttf')
 
 
 words = [(u'标签', 3), (u'云', 2)]
-easywordcloud.draw_word_cloud(words, width, height, path.join(d, 'examples/chinese.png'))
+easywordcloud.draw_word_cloud(words, width, height, path.join(d, '../examples/chinese.png'))
